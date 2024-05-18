5879666eba58fb8da3b27b6f76714e79374d05db
alexandermk04 <alexanderkranz007@gmail.com>
2024-05-18 21:24:28 +0200

    added exceptions in abilities

--- abilities/abilities.py
+++ abilities/abilities.py
@@ -26,44 +26,62 @@ class ShowHelp(BaseAbility):
 
 class ExamAvailability(BaseAbility):
     async def execute(self, recipient, **kwargs):
-        scraper = ExamScraper()
-        if scraper.exams_available():
-            await send_message(recipient, "Prüfungen sind verfügbar!")
-        else:
-            await send_message(recipient, "Keine Prüfungen verfügbar!")
+        try:
+            scraper = ExamScraper()
+            if scraper.exams_available():
+                await send_message(recipient, "Prüfungen sind verfügbar!")
+            else:
+                await send_message(recipient, "Keine Prüfungen verfügbar!")
+        except:
+            await send_message(recipient, "Fehler beim Abrufen der Prüfungen.")
 
 class ExamDates(BaseAbility):
     async def execute(self, recipient, user_message, **kwargs):
-        scraper = ExamScraper()
-        exams = scraper.find_exam(user_message)
+        try:
+            scraper = ExamScraper()
+            exams = scraper.find_exam(user_message)
 
-        if exams:
-            prompt = "Supply the user with the most relevant exam date as well as the name of the following:"
-            exams_string = "\n".join([f"{exam[0]} at {exam[1]}" for exam in exams])
-            #response = prompt_chat(prompt + exams_string, user_message)
-            await send_message(recipient, exams_string)
+            if exams:
+                prompt = "Supply the user with the most relevant exam date as well as the name of the following:"
+                exams_string = "\n".join([f"{exam[0]} at {exam[1]}" for exam in exams])
+                #response = prompt_chat(prompt + exams_string, user_message)
+                await send_message(recipient, exams_string)
+        except:
+            await send_message(recipient, "Fehler beim Abrufen der Prüfungstermine.")
 
 class OOPEvaluation(BaseAbility):
     async def execute(self, recipient, username, **kwargs):
-        await send_message(recipient, f"Suche nach Evaluation für {username}. Dies kann etwas dauern...")
-        response = EvaluationScraper(username).get_evaluation()
-        await send_message(recipient, response)
+        try:
+            await send_message(recipient, f"Suche nach Evaluation für {username}. Dies kann etwas dauern...")
+            response = EvaluationScraper(username).get_evaluation()
+            await send_message(recipient, response)
+        except:
+            await send_message(recipient, "Fehler bei der Evaluationssuche.")
 
 class MealsToday(BaseAbility):
 
     async def execute(self, recipient, **kwargs):
-        meals = MensaScraper("heute").get_meals()
-        await send_message(recipient, meals)
+        try:
+            meals = MensaScraper("heute").get_meals()
+            await send_message(recipient, meals)
+        except:
+            await send_message(recipient, "Fehler beim Abrufen der Mensa-Speisekarte.")
 
 class MealsTomorrow(BaseAbility):
 
     async def execute(self, recipient, **kwargs):
-        meals = MensaScraper("morgen").get_meals()
-        await send_message(recipient, meals)
+        try:
+            meals = MensaScraper("morgen").get_meals()
+            await send_message(recipient, meals)
+        except:
+            await send_message(recipient, "Fehler beim Abrufen der Mensa-Speisekarte.")
 
 class Plan(BaseAbility):
     async def execute(self, recipient, **kwargs):
-        await send_file(recipient, "Hier ist der Campusplan:", PLAN_PATH)
+        try:
+            await send_file(recipient, "Hier ist der Campusplan:", PLAN_PATH)
+        except:
+            await send_message(recipient, "Fehler beim Senden des Campusplans.")
 
 ABILITIES = {"prüfungen verfügbar": ExamAvailability(),
              "prüfungstermine": ExamDates(),
